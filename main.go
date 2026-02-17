641cd48333035c7c91d18805f9e6bb6a4d89487e
BaldozanoEdrian <09301206028edrian@gmail.com>
2026-02-17 23:51:13 +0800

    Solve: I solved how to trigger events using bit flags

--- main.go
+++ main.go
@@ -1,32 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"strconv"
+import "fmt"
 
-	"github.com/Gopher/converter"
+const (
+	//hasSleep   uint64 = 1 << 0
+	hasEaten   uint64 = 1 << 1
+	hasStudied uint64 = 1 << 2
+	//isHappy    uint64 = 1 << 63
 )
 
 func main() {
-	for _, args := range os.Args[1:] {
-		arg, err := strconv.ParseFloat(args, 64)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Converter: %v\n", err)
-		}
 
-		f := converter.Fahrenheit(arg)
-		c := converter.Celsius(arg)
-		fe := converter.Feet(arg)
-		m := converter.Meter(arg)
-		p := converter.Pound(arg)
-		kg := converter.Kilogram(arg)
+	currentState := hasStudied | hasEaten
 
-		fmt.Printf("%s = %s\n", f, converter.CtoF(c))
-		fmt.Printf("%s = %s\n", c, converter.FtoC(f))
-		fmt.Printf("%s = %s\n", fe, converter.MtoFe(m))
-		fmt.Printf("%s = %s\n", m, converter.FetoM(fe))
-		fmt.Printf("%s = %s\n", p, converter.KtoP(kg))
-		fmt.Printf("%s = %s\n", kg, converter.PtoK(p))
+	fmt.Printf("Current State: %d\n", currentState)
+
+	if currentState&hasStudied != 0 {
+		fmt.Println("Edrian is happy even if tired!")
+	} else {
+		fmt.Println("Edrian is disappointed to his self")
 	}
+
 }

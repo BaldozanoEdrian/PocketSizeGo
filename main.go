2c90490544abe942467f17dafa6ec6a8f9e99d59
BaldozanoEdrian <09301206028edrian@gmail.com>
2026-02-19 05:20:17 +0800

    Finally i did solve the duplicate

--- main.go
+++ main.go
@@ -1,24 +1,28 @@
 package main
 
-import "fmt"
-
-const (
-	//hasSleep   uint64 = 1 << 0
-	hasEaten   uint64 = 1 << 1
-	hasStudied uint64 = 1 << 2
-	//isHappy    uint64 = 1 << 63
+import (
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
+	counts := make(map[string]int)
+	scanner := bufio.NewScanner(os.Stdin)
 
-	currentState := hasStudied | hasEaten
-
-	fmt.Printf("Current State: %d\n", currentState)
-
-	if currentState&hasStudied != 0 {
-		fmt.Println("Edrian is happy even if tired!")
-	} else {
-		fmt.Println("Edrian is disappointed to his self")
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "stop" || text == "exit" {
+			break
+		}
+		counts[text]++
 	}
 
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%s\t%d\n", line, n)
+		} else {
+			fmt.Printf("No Duplicate: %s\t%d\n", line, n)
+		}
+	}
 }

074b6c3de2daff2e842ec2905e0d7efa0fb38cdd
BaldozanoEdrian <09301206028edrian@gmail.com>
2026-02-20 04:32:15 +0800

    Feature: The main can now read file given on CLI command

--- main.go
+++ main.go
@@ -7,22 +7,33 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
-	scanner := bufio.NewScanner(os.Stdin)
+	files := os.Args[1:]
 
-	for scanner.Scan() {
-		text := scanner.Text()
-		if text == "stop" || text == "exit" {
-			break
-		}
-		counts[text]++
+	if len(files) == 0 {
+		fmt.Fprintln(os.Stderr, "Error: ")
+		os.Exit(1)
 	}
 
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%s\t%d\n", line, n)
-		} else {
-			fmt.Printf("No Duplicate: %s\t%d\n", line, n)
+	for _, arg := range files {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error opening file %s: %v\n", arg, err)
+			continue
+		}
+
+		defer f.Close()
+
+		scanner := bufio.NewReader(f)
+		for {
+			line, err := scanner.ReadString('\n')
+			if err != nil {
+				if err.Error() == "EOF" {
+					break
+				}
+				fmt.Fprintf(os.Stderr, "Error reading file %s: %v\n", line, err)
+				return
+			}
+			fmt.Println(line)
 		}
 	}
 }

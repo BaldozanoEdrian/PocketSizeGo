4fbe75a2e9752f9b6b02ce2b4a1a00f2261ddf1a
BaldozanoEdrian <09301206028edrian@gmail.com>
2026-02-22 08:49:23 +0800

    Feature: Can find the basename of the src/path

--- main.go
+++ main.go
@@ -1,33 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+
+	"github.com/Gopher/basename"
 )
 
 func main() {
-	files := os.Args[1:]
-
-	if len(files) == 0 {
-		fmt.Printf("Error")
-		os.Exit(1)
-	}
-
-	for _, arg := range files {
-		f, err := os.Open(arg)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error opening file %s: %v\n", arg, err)
-			continue
-		}
-
-		defer f.Close()
-
-		scanner := bufio.NewScanner(f)
-
-		for scanner.Scan() {
-			line := scanner.Text()
-			fmt.Print(line)
-		}
-	}
+	src := "src/path/github.com"
+	fmt.Println(basename.Basename(src))
 }
